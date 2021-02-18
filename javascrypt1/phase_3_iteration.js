bf5758b264b8c552d3b43d2fe3942fcaf01d9b2d
Alec Caro <alec.caro@gmail.com>
2021-02-18 00:01:30 -0500

    bubble

--- javascrypt1/phase_3_iteration.js
+++ javascrypt1/phase_3_iteration.js
@@ -1,6 +1,18 @@
 Array.prototype.bubbleSort = function(){
-   let sorted = false
-   this.forEach(
-       
-   ) 
+   let sorted = false;
+   
+   while (!sorted) {
+      sorted = true;
+
+      for (let i = 0; i < (this.length - 1); i++) {
+         if( this[i] > this[i+1]) {
+            sorted = false;
+            [this[i], this[i + 1]] = [this[i + 1], this[i]];
+         }
+         
+      }
+   }
+   return this;
 }
+
+console.log([4,6,1,8,3,6,2].bubbleSort())
---
+++ javascrypt1/phase_4_recursion.js
@@ -0,0 +1,16 @@
+function itIsUsefulAndThatsGoodHammadSays() {
+   let x = 10;
+   console.log(x);
+   const y = [12];
+   console.log(y);
+   x = 20;
+   console.log(x);
+   y.push(12);
+   console.log(y);
+}
+
+itIsUsefulAndThatsGoodHammadSays();
+
+// useful();
+
+itIsUsefulAndThatsGoodHammadSays();
